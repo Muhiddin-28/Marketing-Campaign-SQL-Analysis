db5319c736795fc9736fdbdf56598641be9fba60
Muhiddin Ahmadov <axmadovmuxiddin@gmail.com>
2026-01-30 22:07:39 +0500

    Delete SQL Script/08_pareto_analysis_by_channel.sql

--- SQL Script/08_pareto_analysis_by_channel.sql
+++
@@ -1,9 +0,0 @@
--- Pareto Analysis by Channel (80/20)
-SELECT
-  channel_used,
-  SUM(roi) AS total_roi,
-  ROUND(SUM(roi) * 100.0 /
-        SUM(SUM(roi)) OVER (),2) AS roi_share
-FROM marketing_campaigns
-GROUP BY channel_used
-ORDER BY total_roi DESC;
